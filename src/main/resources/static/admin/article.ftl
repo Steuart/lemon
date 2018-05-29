3435a0b109b2bc806c25af43b09bcf8baf41e658
HemingWu <jianmo_o@163.com>
2018-05-29 09:46:34 +0800

    编辑器完善

--- src/main/resources/static/admin/article.ftl
+++ src/main/resources/static/admin/article.ftl
@@ -3,7 +3,7 @@
     <head>
         <#include "reference.ftl">
         <link type="text/css" href="/${theme}/css/article.css" rel="stylesheet" />
-        <script type="text/javascript" src="/${theme}/libs/markdown.min.js"></script>
+        <script type="text/javascript" src="/${theme}/libs/showdown.min.js"></script>
         <script type="text/javascript" src="/${theme}/js/article.js"></script>
     </head>
     <body>
@@ -12,9 +12,12 @@
             <p class="head-bar">写文章</p>
             <div class="edit">
                 <div class="write-area">
-                    <textarea id="markdown" oninput="this.editor.update()">请写点什么</textarea>
+                    <textarea id="markdown" oninput="this.editor.update()" onkeydown="tab(this)">请写点什么</textarea>
                 </div>
                 <div class="preview markdown" id="preview"></div>
+            </div>
+            <div class="operate">
+                <i class="fa fa-cog" aria-hidden="true" class="gear"></i>
             </div>
         </div>
     </body>
--- src/main/resources/static/admin/js/article.js
+++ src/main/resources/static/admin/js/article.js
@@ -1,14 +1,35 @@
 $(document).ready(function () {
 
-    function Editor(input, preview) {
-        this.update = function () {
-            preview.innerHTML = markdown.toHTML(input.value);
-        };
-        input.editor = this;
-        this.update();
-    }
+    var converter = new showdown.Converter();
+    showdown.setOption('optionKey', 'value');
     var getIt = function (id) {
         return document.getElementById(id);
     };
-    new Editor(getIt("markdown"), getIt("preview"));
+    new Editor(getIt("markdown"), getIt("preview"),converter);
+
 });
+
+function Editor(input, preview,converter) {
+    this.update = function () {
+        preview.innerHTML = converter.makeHtml(input.value);
+    };
+    input.editor = this;
+    this.update();
+}
+
+
+function tab(obj){
+    var keyCode = event.keyCode;
+    switch(keyCode){
+        case 9:
+            obj.value = obj.value + "    "; // 跳几格由你自已决定
+            event.returnValue = false;
+            break;
+        case 13:
+            obj.value = obj.value + "  \n";
+            event.returnValue = false;
+            break;
+        default:
+
+    }
+}
