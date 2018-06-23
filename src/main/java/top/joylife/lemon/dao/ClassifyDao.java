6aec38865093c44369012f270e84a2985bbc96cf
HemingWu <jianmo_o@163.com>
2018-06-23 11:32:44 +0800

    分类查询

--- src/main/java/top/joylife/lemon/dao/ClassifyDao.java
+++ src/main/java/top/joylife/lemon/dao/ClassifyDao.java
@@ -1,7 +1,38 @@
 package top.joylife.lemon.dao;
 
+import io.swagger.models.auth.In;
+import top.joylife.lemon.dao.entity.Classify;
+
+import java.util.List;
+
 /**
  * created by wuhaiming on 2018/6/20
  */
 public interface ClassifyDao {
+
+    /**
+     * 查询分类列表
+     * @return
+     */
+    List<Classify> list();
+
+    /**
+     * 根据id查询
+     * @return
+     */
+    Classify getById(Integer id);
+
+    /**
+     * 根据id列表查询
+     * @param ids
+     * @return
+     */
+    List<Classify> listByIds(List<Integer> ids);
+
+    /**
+     * 保存
+     * @param classify
+     * @return
+     */
+    Integer save(Classify classify);
 }
--- src/main/java/top/joylife/lemon/dao/impl/ClassifyDaoImpl.java
+++ src/main/java/top/joylife/lemon/dao/impl/ClassifyDaoImpl.java
@@ -1,11 +1,67 @@
 package top.joylife.lemon.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.entity.Example;
 import top.joylife.lemon.dao.ClassifyDao;
+import top.joylife.lemon.dao.entity.Classify;
+import top.joylife.lemon.dao.mapper.ClassifyMapper;
+
+import java.util.List;
 
 /**
  * created by wuhaiming on 2018/6/20
  */
 @Repository
 public class ClassifyDaoImpl implements ClassifyDao {
+
+    @Autowired
+    private ClassifyMapper classifyMapper;
+
+    /**
+     * 查询分类列表
+     *
+     * @return
+     */
+    @Override
+    public List<Classify> list() {
+        return classifyMapper.selectAll();
+    }
+
+    /**
+     * 根据id查询
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public Classify getById(Integer id) {
+        return classifyMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 根据id列表查询
+     *
+     * @param ids
+     * @return
+     */
+    @Override
+    public List<Classify> listByIds(List<Integer> ids) {
+        Example example = new Example(Classify.class);
+        example.createCriteria()
+                .andIn("id",ids);
+        return classifyMapper.selectByExample(example);
+    }
+
+    /**
+     * 保存
+     *
+     * @param classify
+     * @return
+     */
+    @Override
+    public Integer save(Classify classify) {
+        classifyMapper.insertSelective(classify);
+        return classify.getId();
+    }
 }
